96b847221a1ca4d8f65283f36e980992a59942e9
Briscoast <modularconcept.dev@gmail.com>
2024-07-27 17:35:04 +0700

    initial

---
+++ src/main/java/io/modularconcept/conceptzero/ConceptzeroApplication.java
@@ -0,0 +1,12 @@
+package io.modularconcept.conceptzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptzeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptzeroApplication.class, args);
+	}
+}
---
+++ src/test/java/io/modularconcept/conceptzero/ConceptzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.modularconcept.conceptzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptzeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
